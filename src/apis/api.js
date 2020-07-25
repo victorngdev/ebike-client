39e7e6a84a51b10ae76708b9761f2f86c9350d5d
Victor Nguyễn <hoangloi669@gmail.com>
2020-07-26 03:27:53 +0700

    Revert "change server from localhost to heroku"
    This reverts commit 227895d17a5e2aa9dfbe73c1e898705e3ae438ae.

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "http://localhost:8000/api/v1.0"
-    baseURL: "https://cryptic-stream-80614.herokuapp.com/api/v1.0"
+    baseURL: "http://localhost:8000/api/v1.0"
+    // baseURL: "https://cryptic-stream-80614.herokuapp.com/api/v1.0"
 })
